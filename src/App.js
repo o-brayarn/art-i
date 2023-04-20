687c297eb11ca49d6e3ddfdfd0c97908c38b23dc
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-20 21:32:36 +0300

    complete file & folder structure setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Brand, Cta, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatArtI,
+} from "./containers";
 
 const App = () => {
   return (
-    <>
-     
-    </>
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatArtI />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
   );
 };
 
