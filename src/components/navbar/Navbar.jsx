1665d19c4b980b131cb05fcd07de4c14465f9505
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-22 21:28:49 +0300

    edit navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
-import logo from '../../assets/'
+import logo from '../../assets/logo.svg'
 import './navbar.css'
 
 // We use BEM (Block Element Modifier) for our classNames)
@@ -10,6 +10,13 @@ const Navbar = () => {
       <div className='ai__navbar-links'>
         <div className='ai__navbar-links_logo'>
           <img src={logo} alt="logo" />
+        </div>
+        <div className='ai__navbar-links_container'>
+          <p> <a href="#home">Home</a></p>
+          <p> <a href="#wai">What is Art-I</a> </p>
+          <p> <a href="#possibility">Artack</a></p>
+          <p> <a href="#features">Case Studies</a></p>
+          <p> <a href="#blog">Library</a></p>
         </div>
       </div>
     </div>
