d2c63ee682454daf89186ecd6bccff51adc20bfc
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-24 11:28:41 +0300

    create the toggleMenu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 import './navbar.css'
@@ -14,6 +15,7 @@ const Menu = () => {
   </>
 }
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className='ai__navbar'>
       <div className='ai__navbar-links'>
@@ -21,16 +23,31 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className='ai__navbar-links_container'>
-          <p> <a href="#home">Home</a></p>
-          <p> <a href="#wai">What is Art-I</a> </p>
-          <p> <a href="#possibility">Artack</a></p>
-          <p> <a href="#features">Case Studies</a></p>
-          <p> <a href="#blog">Library</a></p>
+         <Menu />
         </div>
       </div>
       <div className='ai__navbar-sign'>
         <p>Sign In</p>
         <button type='button'>SignUp</button>
+      </div>
+
+      <div className='ai__navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+
+        {toggleMenu && (
+          <div className='ai__navbar-menu_container scale-up-center'>
+            <div className='ai__navbar-menu_container-links'>
+              <Menu />
+              <div className='ai__navbar-menu_container-links-sign'>
+              <p>Sign In</p>
+              <button type='button'>SignUp</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
