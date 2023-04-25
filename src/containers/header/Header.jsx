f75bade8b0a1860e50bfd09445744946d76d9a24
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-25 11:07:14 +0300

    create the header content

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,10 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='ai__header section__padding' id='home'>
+      <div className='ai__header-content'>
+        <h1 className='gradient__text'>Welcome to Art-I. We make art worth your while.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque consequuntur nesciunt deserunt ad maiores dolores illum at corporis atque non? Velit pariatur libero aliquam perferendis? Suscipit ad ullam enim quod?</p>
     </div>
   )
 }
