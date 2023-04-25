bfb2bb47845ece9d15b3f01b09924c58884755d8
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-04-25 11:08:16 +0300

    get images

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import './header.css'
-
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 const Header = () => {
   return (
     <div className='ai__header section__padding' id='home'>
       <div className='ai__header-content'>
         <h1 className='gradient__text'>Welcome to Art-I. We make art worth your while.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque consequuntur nesciunt deserunt ad maiores dolores illum at corporis atque non? Velit pariatur libero aliquam perferendis? Suscipit ad ullam enim quod?</p>
+        
         <div className='ai__header-content__input'>
           <input type='email' placeholder='Your email address' />
           <button type='button'>Get Started</button>
         </div>
 
+        <div className='ai__header-content__people'>
+          <img src={people} alt="people" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className='ai__header-image'>
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
     </div>
   )
 }
