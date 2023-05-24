c42ee22b0260c921e78a6700972821343323dbd7
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-24 15:28:44 +0300

    add page content

--- src/containers/whatArtI/WhatArtI.jsx
+++ src/containers/whatArtI/WhatArtI.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './whatArtI.css'
+import {Feature} from "../../components"
 
 const WhatArtI = () => {
   return (
     <div className='ai__whatai section__margin' id='wai'>
-      <div className="ai__whatai-feature"></div>
+      <div className="ai__whatai-feature">
+        <Feature />
+      </div>
+      <div className="ai__whatai-heading">
+        <h1 className="gradient__text">The Possibilities are beyond imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="ai__whatai-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
