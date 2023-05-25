c198e2fc280c4f66b153df09834b3b9c9bc2eebc
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-25 15:01:13 +0300

    add props

--- src/containers/whatArtI/WhatArtI.jsx
+++ src/containers/whatArtI/WhatArtI.jsx
@@ -6,16 +6,16 @@ const WhatArtI = () => {
   return (
     <div className='ai__whatai section__margin' id='wai'>
       <div className="ai__whatai-feature">
-        <Feature />
+        <Feature title="What is Art-I" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius consequatur similique animi, id nulla sint et voluptas ipsa! Consequatur voluptatum minus natus placeat perferendis quisquam necessitatibus ipsum veniam? Repellendus."/>
       </div>
       <div className="ai__whatai-heading">
         <h1 className="gradient__text">The Possibilities are beyond imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className="ai__whatai-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Artistic Fever" text="Lorem ipsum dolor sit amet consectetur adipisicing elit.Itaque eius consequatur similique animi, id nulla sint et voluptas ipsa! Consequatur"/>
+        <Feature title="The Know-How" text="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+        <Feature title="Education" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius consequatur similique animi, id nulla sint et voluptas ipsa! Consequatur voluptatum minus natus placeat perferendis quisquam necess" />
       </div>
     </div>
   )
