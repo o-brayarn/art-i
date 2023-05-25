9b7e4225b8e5936ec131ba36c1b65b02ed77625e
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-25 15:12:56 +0300

    modify: text class

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -9,7 +9,7 @@ const Feature = ({title, text}) => {
         <h1>{title}</h1>
       </div>
       <div className="ai__features-container__feature-text">
-        {text}
+        <p>{text}</p>
       </div>
     </div>
   )
