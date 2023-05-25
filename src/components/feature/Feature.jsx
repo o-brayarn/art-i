baf781b2ac7a5a568ad3da263e25f5bb44a73281
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-25 15:01:46 +0300

    pass props to component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>
-      <h1>Feature</h1>
+    <div className='ai__features-container__feature'>
+      <div className="ai__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="ai__features-container__feature-text">
+        {text}
+      </div>
     </div>
   )
 }
