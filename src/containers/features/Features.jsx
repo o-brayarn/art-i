3f3f20bcdc4a83d3c825b031c99677662c908d4d
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-26 17:20:02 +0300

    complete features page

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './feature.css'
+import {Feature} from "../../components"
+
+const featuresData = [
+  {
+    title: "Lorem Ipsum",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, quas."
+  },
+  {
+    title: "Lorem1 Ipsum",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, quas."
+  },
+  {
+    title: "Lorem3 Ipsum",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, quas."
+  },
+  {
+    title: "Lorem5 Ipsum",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, quas."
+  }
+]
 const Features = () => {
   return (
-    <div>
-     <h1> Features</h1>
+    <div className="ai__features section__padding" id='features'>
+      <div className="ai__features-heading">
+        <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="ai__features-container">
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={ item.text } key={item.title + index} />
+        ))}
+      </div>
     </div>
   )
 }
