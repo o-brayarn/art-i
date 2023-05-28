ecdd4482966c9e75d02e999b76a4c73b263d6d9e
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-28 10:29:16 +0300

    add blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './blog.css'
+import { Article } from '../../components'
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="ai__blog section__padding">
+      <div className="ai__blog-heading">
+        <h1 className='gradient__text'>A lot is happening. We are blogging about it.</h1>
+      </div>
+      <div className="ai__blog-container">
+        <div className="ai__blog-container__groupA">
+          <Article />
+        </div>
+        <div className="ai__blog-container__groupB">
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
     </div>
   )
 }
