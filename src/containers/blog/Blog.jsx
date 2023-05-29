5e4c00ab77aaca656d72f9b45980809af1e95841
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-29 20:19:38 +0300

    complete blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './blog.css'
+import { blog1, blog2, blog3, blog4, blog5 } from './imports'
+
 import { Article } from '../../components'
 const Blog = () => {
   return (
@@ -9,13 +11,13 @@ const Blog = () => {
       </div>
       <div className="ai__blog-container">
         <div className="ai__blog-container__groupA">
-          <Article />
+          <Article imgUrl={blog1} date="Sep 26, 2021" title="This is what you've been waiting for. Let's explore the future!" />
         </div>
         <div className="ai__blog-container__groupB">
-          <Article />
-          <Article />
-          <Article />
-          <Article />
+          <Article imgUrl={blog2} date="Sep 26, 2021" title="This is what you've been waiting for. Let's explore the future!" />
+          <Article imgUrl={blog3} date="Sep 26, 2021" title="This is what you've been waiting for. Let's explore the future!" />
+          <Article imgUrl={blog4} date="Sep 26, 2021" title="This is what you've been waiting for. Let's explore the future!" />
+          <Article imgUrl={blog5} date="Sep 26, 2021" title="This is what you've been waiting for. Let's explore the future!" />
         </div>
       </div>
     </div>
