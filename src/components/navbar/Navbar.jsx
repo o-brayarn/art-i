f57494db78fc3c3d2bd9f9e7da579c8c46520453
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-05-29 21:41:03 +0300

    change logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
-import logo from '../../assets/logo.svg'
 import './navbar.css'
 
 // We use BEM (Block Element Modifier) for our classNames)
@@ -19,7 +18,7 @@ const Navbar = () => {
     <div className='ai__navbar'>
       <div className='ai__navbar-links'>
         <div className='ai__navbar-links_logo'>
-          <img src={logo} alt="logo" />
+          <h1> <span>Art</span>-I</h1>
         </div>
         <div className='ai__navbar-links_container'>
          <Menu />
